1db9b22767b8a49024cddc97854a73c05ee8e7a0
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-10-20 00:50:42 -0600

    this uses list comprehensions to simplify the code for the methods to get even numbers only from a list and to add ! to each string in a list of strings

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    if (num_list == None): return None;
+    elif (len(num_list) < 1): return [];
+    else: return [num for num in num_list if (num % 2 == 0)];
 
 def make_exclamation(sentence_list):
-    pass
+    if (sentence_list == None): return None;
+    elif (len(sentence_list) < 1): return [];
+    else: return [phrase + "!" for phrase in sentence_list];
